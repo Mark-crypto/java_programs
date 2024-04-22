3d9cc7f20882b9b2d4191824adb46c862dfe72b1
Mark-crypto <Mark.Onyango@strathmore.edu>
2024-04-22 12:07:00 +0300

    Factorials

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Factorial {
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.print("Insert number to find its factorial: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(factorial(n));
+    }
+
+    static int factorial(int n) {
+        int ans = 1;
+        if(n == 0){
+            ans = 1;
+        }
+        for (int i = n; i > 0 ; i--) {
+            ans = ans * i;
+        }
+        return ans;
+    }
 }
