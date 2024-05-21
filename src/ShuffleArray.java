ac1d892407eae3b5fbd32cbc4a25b12bc4d82287
Mark-crypto <Mark.Onyango@strathmore.edu>
2024-05-21 11:17:41 +0300

    Shuffle Array

---
+++ src/ShuffleArray.java
@@ -0,0 +1,13 @@
+public class ShuffleArray {
+    public static void main(String[]args){
+    //divide array into two n elements
+        // shuffle such the x is followed by y
+    }
+    static int [] shuffle(int[] nums, int n){
+        int [] arr = new int[n * 2 - 1];
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return nums;
+    }
+}
